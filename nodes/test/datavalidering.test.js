8d49f93c056d80e9675a348da02da28c7a71155f
sune <sune1309@edu.easj.dk>
2020-10-28 09:41:53 +0100

    added some email validty tests

--- nodes/test/datavalidering.test.js
+++ nodes/test/datavalidering.test.js
@@ -1,7 +1,21 @@
 var assert=require('assert');
+var expect = require('chai').expect
 
-describe('AddNeg', function(){
-  it('It should equal 6', function(){
-    assert.strictEqual(6,6)
-  })
-})
+describe('email', function() {
+   it ('skal være gyldig email', function(){
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,6}$/
+    expect(regex.test('test@test.dk')).to.be.true 
+   });
+   it ('skal være ugyldig email - text før @', function(){
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,6}$/
+    expect(regex.test('@test.dk')).to.be.false 
+   });
+   it ('skal være ugyldig email - text mellem @ og top domæne', function(){
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,6}$/
+    expect(regex.test('test@.dk')).to.be.false 
+   });
+   it ('skal være ugyldig email - mangler @', function(){
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,6}$/
+    expect(regex.test('test.dk')).to.be.false 
+   });
+});
