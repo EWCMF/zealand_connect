616140afbd67d278183db7ded91298001a0ac090
Tommy <54972963+EWCMF@users.noreply.github.com>
2020-10-29 09:20:45 +0100

    enpoint tdd

---
+++ nodes/test/database.test.js
@@ -0,0 +1,12 @@
+const mocha = require('mocha');
+const db = require('../persistence/database-connection');
+
+describe("Test database connection", function() {
+    it("should connect to mariadb", function (done) {
+        db.connect('46.101.225.211', 'root', 3306, 'zealand12345').then(conn => {
+            done();
+        });
+    })
+});
+
+describe
