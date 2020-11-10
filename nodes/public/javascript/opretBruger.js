b0be1d1f2845ec7c17b82df2c5cd21f0ba01c3bc
yokarak <54890038+yokarak@users.noreply.github.com>
2020-11-10 10:24:40 +0100

    start commit

---
+++ nodes/public/javascript/opretBruger.js
@@ -0,0 +1,8 @@
+function showVirksomhedButton() {
+    document.getElementById('studerendeKnap').style.display = 'none';
+    document.getElementById('opretFrom').style.display = 'block';
+}
+function showStuderendeButton () {
+    document.getElementById('studerendeKnap').style.display = 'block';
+    document.getElementById('opretForm').style.display = "none";
+}
