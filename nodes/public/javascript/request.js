2712cd960e8972c840a648c8db035f3200d9bec6
yokarak <54890038+yokarak@users.noreply.github.com>
2020-11-12 11:32:36 +0100

    forkortet / fjernet unødvendigt

--- nodes/public/javascript/request.js
+++ nodes/public/javascript/request.js
@@ -1,36 +1,8 @@
 var reqLang = function(req, res) {
     temp = 'da'
-    
     if(req.cookies.lang == 'en') {
         temp = 'en';
-        console.log('yeah')
-    } 
-
-
-    
-    
+    }    
     return temp
 }
-
-var dd = function() {
-     if(req.cookies.lang != null) {
-        temp = req.cookies.lang
-    }
-    
-
-    else if(req.cookies.lang == null) {
-        temp = req.acceptsLanguages('da', 'en');
-        if(temp == null) {
-            temp = 'da'
-        }    
-        res.cookie(temp, req.params.lang)
-    }
-    
-
-    if (temp != 'da' || 'en') {
-        temp = 'da'
-    }
-}
-
-
 module.exports.reqLang = reqLang;
