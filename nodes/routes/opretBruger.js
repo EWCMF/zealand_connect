d13b874502c421030b0ede8ac7378a640fd7b6b6
Benjamin Kyhn <benkyhn@hotmail.com>
2020-11-13 13:01:03 +0100

    Opret bruger route opdateret

--- nodes/routes/opretBruger.js
+++ nodes/routes/opretBruger.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var { reqLang } = require('../public/javascript/request');
-
+const models = require('../models');
+const createVirksomhed = require('../persistence/usercrud').createVirksomhed;
+const deleteVirksomhed = require('../persistence/usercrud').deleteVirksomhed;
 
 /* GET login page. */
 router.get('/', function (req, res, next) {
-res.render('opretBruger', {language: reqLang(req)})
-    
+    res.render('opretBruger', {language: reqLang(req)})
+
 });
+
+router.post('/create', function (req, res) {
+    createVirksomhed("memeboi@nielsen.dk")
+    res.redirect('back');
+});
+
+router.post('/delete', function (req, res) {
+    deleteVirksomhed("memeboi@nielsen.dk")
+    res.redirect('back');
+})
 
 module.exports = router;
