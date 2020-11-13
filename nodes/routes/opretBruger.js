1e22326adab1746034b3eb60802187ad3a7afa5e
Benjamin Kyhn <benkyhn@hotmail.com>
2020-11-13 10:54:47 +0100

    Testing some sheit

--- nodes/routes/opretBruger.js
+++ nodes/routes/opretBruger.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var { reqLang } = require('../public/javascript/request');
-
+const models = require('../models');
 
 /* GET login page. */
 router.get('/', function (req, res, next) {
@@ -9,8 +9,16 @@ res.render('opretBruger', {language: reqLang(req)})
 
 });
 
-router.post('/create', function (req, res) {
-    console.log("A memeboi was created")
+router.post('/create', async function (req, res) {
+    try {
+        const memeboi = await models.Virksomhed.create({email: "memeboi@nielsen.dk"});
+        console.log("A memeboi was created");
+        // console.log(memeboi instanceof Virksomhed);
+        // console.log(memeboi.email);
+    } catch (e) {
+        console.log(e);
+    }
+
 });
 
 module.exports = router;
