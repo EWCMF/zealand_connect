9a6ff84e78e1a29fd2da739cb633e9c4c8d2b1dc
Benjamin Kyhn <benkyhn@hotmail.com>
2020-11-13 10:11:03 +0100

    Tilføjet en /opretBruger/create route, som tager imod en post request

--- nodes/routes/opretBruger.js
+++ nodes/routes/opretBruger.js
@@ -6,7 +6,11 @@ var { reqLang } = require('../public/javascript/request');
 /* GET login page. */
 router.get('/', function (req, res, next) {
 res.render('opretBruger', {language: reqLang(req)})
-    
+
+});
+
+router.post('/create', function (req, res) {
+    console.log("A memeboi was created")
 });
 
 module.exports = router;
