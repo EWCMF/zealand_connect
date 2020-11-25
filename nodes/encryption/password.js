e6d61de49125bde36ad6210a68a413cde87b1b8b
Benjamin Kyhn <benkyhn@hotmail.com>
2020-11-25 10:32:32 +0100

    Lavet en hashPassword funktion

--- nodes/encryption/password.js
+++ nodes/encryption/password.js
@@ -3,21 +3,23 @@ const bcrypt = require('bcryptjs');
 //Todo: encrpytion password function
 
 
-
-async function verifyPassword(htmlPwd, dbPwd){
-    return new Promise(resolve =>{
+async function verifyPassword(htmlPwd, dbPwd) {
+    return new Promise(resolve => {
         console.log('Trying to verify password from login:');
-        bcrypt.compare(htmlPwd,dbPwd, function(err, result){
-        if(result==true){
-            console.log('--- PASSWORD MATCH THE DATABASE PASSWORD---');
-            resolve(true);
-        } else {
-            console.log('passowrds did not match'+ htmlPwd);
-            resolve(false);
-        }
-    })
+        bcrypt.compare(htmlPwd, dbPwd, function (err, result) {
+            if (result == true) {
+                console.log('--- PASSWORD MATCH THE DATABASE PASSWORD---');
+                resolve(true);
+            } else {
+                console.log('password did not match' + htmlPwd);
+                resolve(false);
+            }
+        })
     })
 }
 
+async function hashPassword(password) {
+    return await bcrypt.hash(password, 10);
+}
 
-module.exports = {verifyPassword: verifyPassword}
+module.exports = {verifyPassword: verifyPassword, hashPassword: hashPassword}
