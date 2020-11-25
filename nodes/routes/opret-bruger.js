4541b6f25482575415442ac35d6d46f40a4cba72
Benjamin Kyhn <benkyhn@hotmail.com>
2020-11-25 13:00:50 +0100

    Validering af email og cvr

--- nodes/routes/opret-bruger.js
+++ nodes/routes/opret-bruger.js
@@ -7,24 +7,51 @@ const hashPassword = require('../encryption/password').hashPassword;
 
 /* GET login page. */
 router.get('/', function (req, res, next) {
+    let error = req.query;
+    console.log(error);
+    let msg = error.error;
+    console.log(msg);
+
     res.render('opret-bruger', {language: reqLang(req)})
 });
 
 router.post('/create', function (req, res) {
-    hashPassword(req.body.password).then((hashedPassword) => {
-        let virksomhedsBruger = {
-            email: req.body.email,
-            password: hashedPassword,
-            tlfnr: req.body.telefonnummer,
-            by: req.body.by,
-            postnr: req.body.postnummer,
-            cvrnr: req.body.cvr
-        }
+    let email = req.body.email;
+    let password = req.body.password;
+    let tlfnr = req.body.telefonnummer;
+    let by = req.body.by;
+    let postnr = req.body.postnummer;
+    let cvrnr = req.body.cvr;
 
-        createVirksomhed(virksomhedsBruger);
-    });
+    let error = "error=";
 
-    res.redirect('back');
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,6}$/
+    let correctEmail = emailRegex.test(email);
+
+    if (!correctEmail){
+        error += "incorrectemail";
+        res.redirect('/opret-bruger' + '?' + error);
+    }
+
+    // if (cvrnr.length !== 8){
+    //     error += "cvrlengtherror";
+    //     res.redirect('/opret-bruger' + '?' + error);
+    // }
+
+    // hashPassword(req.body.password).then((hashedPassword) => {
+    //     let virksomhedsBruger = {
+    //         email: req.body.email,
+    //         password: hashedPassword,
+    //         tlfnr: req.body.telefonnummer,
+    //         by: req.body.by,
+    //         postnr: req.body.postnummer,
+    //         cvrnr: req.body.cvr
+    //     }
+    //
+    //     createVirksomhed(virksomhedsBruger);
+    // });
+    //
+    // res.redirect('back');
 });
 
 router.post('/delete', function (req, res) {
