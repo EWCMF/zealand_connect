9eed6bce14405748d35a9fe53477eb50d5157fe5
Matias <54976149+Matiaslys@users.noreply.github.com>
2020-11-25 11:02:55 +0100

    PDF kit og hvad jeg har lavet indtil videre

--- nodes/routes/search-cv.js
+++ nodes/routes/search-cv.js
@@ -252,5 +252,79 @@ router.get('/:id', function (req, res) {
 
 });
 
+router.get('/:id/Create_pdf', function (req, res, next) {
+    let id = req.params.id
+
+    // https://pdfkit.org/docs/guide.pdf
+    // http://pdfkit.org/
+  var pdf = require('pdfkit');
+  var fs = require('fs');
+  
+  var myDoc = new pdf;
+  var pdfStream = fs.createWriteStream('PDF/Test.pdf')
+  myDoc.pipe(pdfStream);
+  console.log(id);
+  // var design = require('../Scripts/design_af_CV');
+  db.CV.findOne({
+    raw: true,
+    where: {
+        id: parseInt(id)
+    }
+}).then((cv) => {
+    myDoc.image('public/images/facebook-default-no-profile-pic.jpg', 40, 40, {width: 150, height: 150})
+  
+    myDoc.font('Times-Roman')
+        .fontSize(24)
+        .text('Navn Navnesen',220,50)
+    
+    myDoc.font('Times-Roman')
+        .fontSize(10.72)
+        .text(cv.email,220,90)
+
+    myDoc.font('Times-Roman')
+        .fontSize(10.72)
+        .text(cv.telefon,220,120)
+
+    myDoc.font('Times-Roman')
+        .fontSize(10.72)
+        .text(cv.hjemmeside,220,150)
+
+    myDoc.font('Times-Roman')
+        .fontSize(10.72)
+        .text(cv.linkedIn,220,180)
+
+    myDoc.moveTo(0,210)
+        .lineTo(612,210)
+        .stroke()
+
+    // myDoc.font('Times-Roman')
+    //     .fontSize(10.72)
+    //     .text(cv.om_mig,220,180)
+    myDoc.end()
+});
+    pdfStream.addListener('finish', function() {
+      res.setHeader('content-type', 'application/pdf'),
+      res.download('PDF/Test.pdf', 'Testpdf.pdf')
+    });
+  
+    async function deleteFile() {
+  try {
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => resolve(
+      fs.unlinkSync('PDF/Test.pdf', (err) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+      })), 1000)
+    });
+    await promise;
+  } catch (error) {
+    console.log(error)
+  }}
+  
+    deleteFile();
+  });
+
 
 module.exports = router;
