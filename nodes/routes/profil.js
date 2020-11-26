1a48cb5dd48f917555fd56b9c81d564ab28255fb
Rasmus Jannerup <rasm211d@edu.easj.dk>
2020-11-26 12:11:31 +0100

    Ruten til at differenciere mellem student og virksomhed når man er logget ind virker nu.

--- nodes/routes/profil.js
+++ nodes/routes/profil.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+const findUserByEmail = require('../persistence/usermapping').findUserByEmail;
 
 router.get('/', function (req, res, next) {
-    res.send("PROFIl: "+req.user);
+    findUserByEmail(req.user).then((user) => {
+        if (user instanceof models.Virksomhed) {
+            //TODO: Her skal der være virksomhedsprofil
+            res.send("HER SKAL DER VÆRE VIRKSOMHEDSPROFIL");
+        } else if (user instanceof models.Student) {
+            res.render("studentprofil");
+        }
+    }); 
 });
 
 router.get('/rediger', function (req, res, next) {
-    //todo: find bruger og insæt dens data i render hbs.
     res.render("rediger-virksomhedsprofil");
+    //todo: find bruger og insæt dens data i render hbs.
 });
 
 module.exports = router;
