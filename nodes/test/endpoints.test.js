aa24b51094f2fac33578e6a3a5ec1b12866371ac
Benjamin Kyhn <benkyhn@hotmail.com>
2020-11-26 16:27:16 +0100

    Test til POST på /opret-bruger/create

--- nodes/test/endpoints.test.js
+++ nodes/test/endpoints.test.js
@@ -2,7 +2,10 @@ const request = require('supertest');
 const express = require('express');
 const app = require('../app');
 const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = require('chai').expect
 
+chai.use(chaiHttp);
 
 /*describe('GET /', function() {
     it('respond with hello world', function(done) {
@@ -12,13 +15,13 @@ const chai = require('chai');
 });*/
 
 describe('GET /users', function () {
-    it('respond with status code OK 200', function(done) {
+    it('respond with status code OK 200', function (done) {
         request(app).get('/users').expect(200, done);
     })
 });
 
 describe('GET /mit-CV', function () {
-    it('respond with status code OK 200', function(done) {
+    it('respond with status code OK 200', function (done) {
         request(app).get('/mit-CV').expect(200, done);
     })
 });
@@ -31,14 +34,36 @@ describe('GET /mit-CV', function () {
 // });
 
 describe('GET /login', function () {
-    it('respond with status code OK 200', function(done) {
+    it('respond with status code OK 200', function (done) {
         request(app).get('/login').expect(200, done);
     })
 });
 
-describe('GET /opret-bruger', function() {
+describe('GET /opret-bruger', function () {
     it('respond with status code OK 200', function (done) {
         request(app).get('/opret-bruger').expect(200, done);
     })
+});
+
+describe('POST /opret-bruger/create', () => {
+    it('it should meme', (done) => {
+        let virksomhed = {
+            email: "test@test.dk",
+            gentagEmail: "test@test.dk",
+            password: "12345678",
+            gentagPassword: "12345678",
+            telefonnummer: "12345678",
+            by: "Testby",
+            postnummer: "0000",
+            cvr: "12345678"
+        }
+        chai.request(app)
+            .post('/opret-bruger/create')
+            .send(virksomhed)
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
 });
 
