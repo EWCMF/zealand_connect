8fb9bb605d8c41cbdb32acee3e2bc9a9aec427fb
simo215d <simo215d@edu.easj.dk>
2020-11-26 10:04:20 +0100

    fejlbeskeder færdige

--- nodes/routes/profil.js
+++ nodes/routes/profil.js
@@ -9,16 +9,20 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/rediger', function (req, res, next) {
-    let error = req.query.error;
+    let errors = req.query;
+    console.log("ERRORS");
+    console.log(errors);
+    
     //todo if Student render student else virksomhed
     //todo: find bruger og indsæt dens data i render hbs.
     findUserByEmail(req.user).then((user)=>{
         if(user instanceof models.Student){
             res.send("not implemented yet mate");
         } else {
-            res.render("rediger-virksomhedsprofil", {error: error, email: user.email, tlfnr: user.tlfnr, by: user.by, postnummer:user.postnr, cvr: user.cvrnr, firmanavn: user.navn, adresse: user.adresse, hjemmeside: user.hjemmeside, direktoer: user.direktoer, land: user.land});
+            //render with potential errors and information about the profile
+            res.render("rediger-virksomhedsprofil", {EmailError: errors.EmailError, TlfnrError: errors.TlfnrError, ByError: errors.ByError, PostnrError: errors.PostnrError, CVRError: errors.CVRError, NavnError: errors.NavnError, AdresseError: errors.AdresseError, HjemmesideError: errors.HjemmesideError, DirektoerError: errors.DirektoerError, LandError: errors.LandError, LogoError: errors.LogoError, email: user.email, tlfnr: user.tlfnr, by: user.by, postnummer:user.postnr, cvr: user.cvrnr, firmanavn: user.navn, adresse: user.adresse, hjemmeside: user.hjemmeside, direktoer: user.direktoer, land: user.land});
         }
-    })
+    });
 });
 
 router.post('/rediger-save', function (req, res, next) {
@@ -36,26 +40,35 @@ router.post('/rediger-save', function (req, res, next) {
     let direktoer = req.body.direktoer;
     let land = req.body.land;
     //todo logo
-    let error = "?error=";
+    let errors = {
+        EmailError: "",
+        TlfnrError: "",
+        ByError: "",
+        PostnrError: "",
+        CVRError: "",
+        NavnError: "",
+        AdresseError: "",
+        HjemmesideError: "",
+        DirektoerError: "",
+        LandError: "",
+        LogoError: "",
+    }
     if (!validation.validateCVR(cvrnr)){
-        error += "invalidcvrerror";
-        res.redirect('/profil/rediger' + error);
+        errors.CVRError = "Ugyldigt CVR";
     }
 
     else if (!validation.validateCvrLength(cvrnr)){
-        error += "cvrlengtherror";
-        res.redirect('/profil/rediger' + error);
+        errors.CVRError = "Ugyldigt CVR længde";
     }
 
     if (!validation.validatePhone(tlfnr)){
-        error += "invalidphoneerror";
-        res.redirect('/profil/rediger' + error);
+        errors.TlfnrError = "Ugyldigt Telefonnummer";
     }
 
     if (!validation.validateCity(by)){
-        error += "invalidbyerror";
-        res.redirect('/profil/rediger' + error);
+        errors.ByError = "Ugyldig By";
     }
+    res.redirect('/profil/rediger?'+'EmailError='+errors.EmailError+'&TlfnrError='+errors.TlfnrError+'&ByError='+errors.ByError+'&PostnrError='+errors.PostnrError+'&CVRError='+errors.CVRError+'&NavnError='+errors.NavnError+'&AdresseError='+errors.AdresseError+'&HjemmesideError='+errors.HjemmesideError+'&DirektoerError='+errors.DirektoerError+'&LandError='+errors.LandError+'&LogoError='+errors.LogoError);
 });
 
 module.exports = router;
