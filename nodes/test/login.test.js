e0d724a09268c9e9b5cad1e920fc66ab2b6a482c
simo215d <simo215d@edu.easj.dk>
2020-11-26 13:42:56 +0100

    har tilføjet folk fra klaseen som seed til students tabellen og ændret test ved login

--- nodes/test/login.test.js
+++ nodes/test/login.test.js
@@ -1,8 +1,6 @@
 var assert = require('assert');
 const findUserByEmail = require('../persistence/usermapping').findUserByEmail;
-// const findUserById = require('../persistence/usermapping').findUserById;
 const verifyPassword = require('../encryption/password').verifyPassword;
-
 
 describe('Find student i database baseret på email', function(){
     //TC1
@@ -23,11 +21,10 @@ describe('Find student i database baseret på email', function(){
     })  
 })
 
-
 describe('Find bruger i database baseret på email', function(){
     //TC3
     it('Bør find en Virksomheds bruger med email som matcher det username vi indtaster', function(done){
-        let email = 'kontakt@bigfoods.dk';
+        let email = 'kontakt@maersk.com';
         findUserByEmail(email).then((virksomhed)=>{
             assert.strictEqual(virksomhed.email, email);
             done();
@@ -42,29 +39,6 @@ describe('Find bruger i database baseret på email', function(){
         })
     })  
 })
-
-
-/*
- describe('Find bruger i database baseret på id', function(){
-    //TC5
-    it('Bør ikke kunne finde en bruger hvis ingen bruger med denne id findes', function(done){
-        let id = -1;
-        findUserById(id).then((user)=>{
-            assert.strictEqual(user, null);
-            done();
-        })
-    })
-    //TC6
-    it('Bør find en bruger med id som matcher det id vi indtaster', function(done){
-        let id = 1;
-        findUserById(id).then((user)=>{
-            assert.strictEqual(user.id, id);
-            done();
-        })
-    })
-})
-*/
-
 
 describe('Check om 2 password er ens', function(){
     //TC7
@@ -86,29 +60,3 @@ describe('Check om 2 password er ens', function(){
         })
     })
 })
-
-/*
-
-describe('Opret en googlebruger i systemet og så check at den eksisterer', function(){
-    it('Bør oprette google bruger og kunne finde brugeren baseret på username', function(done){
-        let username = 'benny';
-        let googleID = 69420;
-        createGoogleUser(username, googleID).then(()=>{
-            findUserByName(username).then((user)=>{
-                assert.strictEqual(user.username, username);
-                done();
-            })
-        });
-    })
-    
-    it('Bør oprette zealand bruger og kunne finde brugeren baseret på username', function(done){
-        let username = 'benny';
-        let password = 'blabla123';
-        createZealandUser(username, password).then(()=>{
-            findUserByName(username).then((user)=>{
-                assert.strictEqual(user.username, username);
-                done();
-            }) 
-        })
-    }) 
-})*/ 
