328696c28a07595ab45064e64f5cfe581bb7bacd
Mildrloll <42993951+Mildrloll@users.noreply.github.com>
2020-11-27 13:19:38 +0100

    Lavet en forside line og noget text.

--- nodes/routes/forside.js
+++ nodes/routes/forside.js
@@ -2,10 +2,9 @@ var express = require('express');
 var router = express.Router();
 const { reqLang } = require('../public/javascript/request');
 
-
 router.get('/', function (req, res, next) {
-    res.render('forside', {
-      language: reqLang(req ,res)
-    })
-  });
-  module.exports=router;
+  res.render('forside', {
+    language: reqLang(req, res)
+  })
+});
+module.exports = router;
