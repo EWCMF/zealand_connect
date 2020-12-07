33fa70d3baca9b2e3b2dc9db2fb61cf3832e33b3
Anders Bille <anders.bille.lind@gmail.com>
2020-12-07 16:25:18 +0100

    alt undtaget tlfError virker
    hver anden gang siger der er error

--- nodes/constants/regex.js
+++ nodes/constants/regex.js
@@ -1,6 +1,6 @@
 const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,6}$/
 const cvrRegex = /^[0-9]{8}$/
-const phoneRegex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/
+const phoneRegex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g
 const cityRegex = /^[a-zA-Z æøåÆØÅ]*$/
 const dateRegex = /^\d{4}[./-]\d{2}[./-]\d{2}$/;
 const linkRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
