c852e5d5cce8ca11f850ac389701bd17fa9d06dc
Anders Bille <anders.bille.lind@gmail.com>
2020-12-08 14:56:22 +0100

    renskrivning

--- nodes/persistence/uddanelsemapping.js
+++ nodes/persistence/uddanelsemapping.js
@@ -3,30 +3,33 @@ const models = require("../models");
 async function createUddanelse(nameobj) {
     if ( await findUddannelseByName(nameobj) === null) {
     try {
-      const uddannelse = await models.Uddannelser.create({
+      await models.Uddannelser.create({
         name: nameobj,
       });
+      return "uddannelseOprettet";
     } catch (e) {
       console.log(e);
     }
+  } else{
+      return "uddannelseFindesAllerede";
   }
 }
 
 async function findUddannelseByName(name) {
-  let user = null;
+  let udd = null;
   return new Promise((resolve) => {
     console.log("--finding uddannelse by Name: " + name + "---");
     models.Uddannelser.findOne({
       where: { name: name },
-    }).then((uddannelser) => {
-      if (uddannelser === null) {
+    }).then((uddannelse) => {
+      if (uddannelse === null) {
         console.log("en uddannelse med dette navn findes ikke!");
       }
-      if (uddannelser instanceof models.Uddannelser) {
+      if (uddannelse instanceof models.Uddannelser) {
         console.log("--- fandt en uddannelse med navnet ---");
-        user = uddannelser;
+        udd = uddannelse;
       }
-      resolve(user);
+      resolve(udd);
     });
   });
 }
