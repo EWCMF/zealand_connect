33880c9e7c1204902d3ced06f5039c93f8a94ca5
Anders Bille <anders.bille.lind@gmail.com>
2020-12-09 09:49:23 +0100

    retter mærkeligt path navn

--- nodes/routes/opret-bruger.js
+++ nodes/routes/opret-bruger.js
@@ -26,7 +26,7 @@ router.get('/', function (req, res, next) {
     });
 });
 
-router.post('/createErrors', (req, res) => {
+router.post('/create', (req, res) => {
 
 
      // Indlæs variable fra viewet
