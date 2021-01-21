89d93a5697e3adec62655e35326548abef6d2960
Benjamin Kyhn <benkyhn@hotmail.com>
2021-01-21 15:18:49 +0100

    Lav opslag indlæser data fra virksomhedens bruger

--- nodes/routes/internship_post.js
+++ nodes/routes/internship_post.js
@@ -7,13 +7,13 @@ var fs = require("fs"); //Bruges til grundlæggen file hændtering.
 var mv = require('mv'); //Skal bruges for kunne gemme uploads uden for container.
 var uploadFolder = require('../constants/references').uploadFolder()
 const {
-  emailRegex,
-  dateRegex,
-  linkRegex
+    emailRegex,
+    dateRegex,
+    linkRegex
 } = require("../constants/regex.js");
 const db = require('../models');
 const {
-  REPL_MODE_SLOPPY
+    REPL_MODE_SLOPPY
 } = require('repl');
 const findUserByEmail = require('../persistence/usermapping').findUserByEmail;
 const models = require("../models");
@@ -21,240 +21,266 @@ var tempDate = dateRegex.source;
 var tempEmail = emailRegex.source;
 var tempLink = linkRegex.source;
 
-/* POST home page. */
 router.post('/', function (req, res, next) {
-      //For at håndtere filupload og almindelige input data på tid skal man parse req igennem formidable.
-      var formData = new formidable.IncomingForm();
-      formData.parse(req, function (error, fields, files) {
+    //For at håndtere filupload og almindelige input data på tid skal man parse req igennem formidable.
+    var formData = new formidable.IncomingForm();
+    formData.parse(req, function (error, fields, files) {
         //laver et objekt med alle data
         var {
-          title,
-          post_type,
-          email,
-          contact,
-          education,
-          country,
-          post_start_date,
-          post_end_date,
-          post_text,
-          city,
-          postcode,
-          company_link,
-          post_document,
-          dawa_json,
-          dawa_uuid,
-          expired
+            title,
+            post_type,
+            email,
+            contact,
+            education,
+            country,
+            post_start_date,
+            post_end_date,
+            post_text,
+            city,
+            postcode,
+            company_link,
+            post_document,
+            dawa_json,
+            dawa_uuid,
+            expired
         } = fields;
 
         var region = '';
 
         if (country == 1) {
-          postcode = Number(postcode);
-          var xmlhttp = new XMLHttpRequest();
-          xmlhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-              var json = JSON.parse(this.responseText);
-              region = json[0].adgangsadresse.region.navn;
-            }
-          };
-          xmlhttp.open("GET", "https://dawa.aws.dk/adresser?id=" + dawa_uuid, false);
-          xmlhttp.setRequestHeader("Content-type", "application/json");
-          xmlhttp.send();
+            postcode = Number(postcode);
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    var json = JSON.parse(this.responseText);
+                    region = json[0].adgangsadresse.region.navn;
+                }
+            };
+            xmlhttp.open("GET", "https://dawa.aws.dk/adresser?id=" + dawa_uuid, false);
+            xmlhttp.setRequestHeader("Content-type", "application/json");
+            xmlhttp.send();
         } else {
-          // sæt adresse feltets data til tomme strings hvis der er valgt et andet land end danmark
-          city = '';
-          postcode = 0;
-          dawa_json = '';
-          dawa_uuid = '';
+            // sæt adresse feltets data til tomme strings hvis der er valgt et andet land end danmark
+            city = '';
+            postcode = 0;
+            dawa_json = '';
+            dawa_uuid = '';
         }
 
         var indhold = {
-          title,
-          post_type,
-          email,
-          contact,
-          education,
-          country,
-          region,
-          post_start_date,
-          post_end_date,
-          post_text,
-          city,
-          postcode,
-          company_link,
-          post_document,
-          dawa_json,
-          dawa_uuid,
-          expired
+            title,
+            post_type,
+            email,
+            contact,
+            education,
+            country,
+            region,
+            post_start_date,
+            post_end_date,
+            post_text,
+            city,
+            postcode,
+            company_link,
+            post_document,
+            dawa_json,
+            dawa_uuid,
+            expired
         };
 
         var inputError = false;
 
         //Test inputfelterne hvis javascript er deaktiveret af sikkerhedsmæssige årsager
         if (1 > title.length || title.length > 255) {
-          console.log('Title length invalid');
-          inputError = true;
+            console.log('Title length invalid');
+            inputError = true;
         }
 
         if (post_type == 0) {
-          console.log('Missing type');
-          inputError = true;
+            console.log('Missing type');
+            inputError = true;
         }
 
         if (country == 0) {
-          console.log('Missing country');
-          inputError = true;
+            console.log('Missing country');
+            inputError = true;
         }
 
         if (country == 1 && dawa_uuid == '') {
-          console.log('Missing address');
-          inputError = true;
+            console.log('Missing address');
+            inputError = true;
         }
 
         if (email.length > 255) {
-          console.log('Email to long');
-          inputError = true;
+            console.log('Email to long');
+            inputError = true;
         }
 
         if (!emailRegex.test(email)) {
-          console.log('Invalid email');
-          inputError = true;
+            console.log('Invalid email');
+            inputError = true;
         }
 
         if (1 > contact.length || contact.length > 255) {
-          console.log('Contact length invalid');
-          inputError = true;
+            console.log('Contact length invalid');
+            inputError = true;
         }
 
         if (!dateRegex.test(post_start_date)) {
-          console.log('Invalid date');
-          inputError = true;
+            console.log('Invalid date');
+            inputError = true;
         } else {
-          let currDate = new Date();
-          let inputDate = new Date(post_start_date);
-    
-          if (currDate > inputDate) {
-            console.log('Invalid date');
-            inputError = true;
-          }
-        }
-    
-        if (post_type == 1) {
-          if (!dateRegex.test(post_end_date)) {
-            console.log('Invalid date');
-            inputError = true;
-          } else {
             let currDate = new Date();
-            let inputDate = new Date(post_end_date);
-      
+            let inputDate = new Date(post_start_date);
+
             if (currDate > inputDate) {
-              console.log('Invalid date');
-              inputError = true;
+                console.log('Invalid date');
+                inputError = true;
             }
-          }
+        }
+
+        if (post_type == 1) {
+            if (!dateRegex.test(post_end_date)) {
+                console.log('Invalid date');
+                inputError = true;
+            } else {
+                let currDate = new Date();
+                let inputDate = new Date(post_end_date);
+
+                if (currDate > inputDate) {
+                    console.log('Invalid date');
+                    inputError = true;
+                }
+            }
         } else {
-          indhold.post_end_date = null;
+            indhold.post_end_date = null;
         }
 
         if (post_text.length > 65536) {
-          console.log('Plain text is to long');
-          inputError = true;
+            console.log('Plain text is to long');
+            inputError = true;
         }
 
         if (company_link != '') {
-          if (!linkRegex.test(company_link)) {
-            console.log("Link Invalid");
-            inputError = true;
-          }
+            if (!linkRegex.test(company_link)) {
+                console.log("Link Invalid");
+                inputError = true;
+            }
         } else {
-          indhold.company_link = null;
+            indhold.company_link = null;
         }
 
         if (education == 0) {
-          console.log('Invalid choice');
-          inputError = true;
+            console.log('Invalid choice');
+            inputError = true;
         }
 
         //Database kode må først køre efter flyttelses og omdøb af uploadet filer er fuldført.
         async function dbExe() {
-          //checkbox_state();
-          if (!inputError) {
-            let user = res.locals.user;
-            let company_id = user.id;
+            //checkbox_state();
+            if (!inputError) {
+                let user = res.locals.user;
+                let company_id = user.id;
 
-            indhold.fk_company = company_id;
+                indhold.fk_company = company_id;
 
-            const post = await db.InternshipPost.create(indhold).catch((error) => {
-              console.log(error);
-              return res.status(400).send(error);
-            });
-            res.redirect('../internship_view/' + post.id)
-          }
+                const post = await db.InternshipPost.create(indhold).catch((error) => {
+                    console.log(error);
+                    return res.status(400).send(error);
+                });
+                res.redirect('../internship_view/' + post.id)
+            }
         }
 
         if (files.post_document.size === 0) {
-          dbExe();
-        } else {
-          /*fileUpload here*/
-          var doc = files.post_document;
-
-          //Stien til upload mappen skal være til stien i docker containeren.
-          var publicUploadFolder = uploadFolder;
-
-          //Generere unik data til filnavn med Date.now() og tilfældig tal.
-          var datetime = Date.now();
-
-          var randomNumber = Math.floor(Math.random() * (10 - 0 + 1) + 0);
-
-          //Kombinere oprindelig filnavn med unik data for at lave unike filnavne.
-          var newDocName = datetime + randomNumber + "_" + doc.name;
-
-          if (doc.size <= 10240000) {
-            //Når filer bliver uploaded bliver de lagt i en midlertigt mappe med tilfældignavn.
-            //Nedenstående flytter og omdøber filer på sammetid
-            if (doc.type == "text/plain" || doc.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document" || doc.type == "application/pdf" || doc.type == "application/msword") {
-              mv(doc.path, publicUploadFolder + newDocName, (errorRename) => {
-
-                if (errorRename) {
-                  console.log("Unable to move file.");
-                } else {
-                  indhold.post_document = newDocName;
-                }
-                dbExe();
-              });
-            } else {
-              console.log("invalid file");
-              dbExe();
-            }
-          } else {
-            console.log("invalid filesize");
             dbExe();
-          }
+        } else {
+            /*fileUpload here*/
+            var doc = files.post_document;
+
+            //Stien til upload mappen skal være til stien i docker containeren.
+            var publicUploadFolder = uploadFolder;
+
+            //Generere unik data til filnavn med Date.now() og tilfældig tal.
+            var datetime = Date.now();
+
+            var randomNumber = Math.floor(Math.random() * (10 - 0 + 1) + 0);
+
+            //Kombinere oprindelig filnavn med unik data for at lave unike filnavne.
+            var newDocName = datetime + randomNumber + "_" + doc.name;
+
+            if (doc.size <= 10240000) {
+                //Når filer bliver uploaded bliver de lagt i en midlertigt mappe med tilfældignavn.
+                //Nedenstående flytter og omdøber filer på sammetid
+                if (doc.type == "text/plain" || doc.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document" || doc.type == "application/pdf" || doc.type == "application/msword") {
+                    mv(doc.path, publicUploadFolder + newDocName, (errorRename) => {
+
+                        if (errorRename) {
+                            console.log("Unable to move file.");
+                        } else {
+                            indhold.post_document = newDocName;
+                        }
+                        dbExe();
+                    });
+                } else {
+                    console.log("invalid file");
+                    dbExe();
+                }
+            } else {
+                console.log("invalid filesize");
+                dbExe();
+            }
         }
-      });
+    });
 });
 
-/* GET home page. */
-router.get('/', function (req, res, next) { 
+router.get('/', async function (req, res, next) {
+    let loggedInVirksomhed;
 
-      var generatedEducationOptions = '';
+    await findUserByEmail(req.user).then((user) => {
+        loggedInVirksomhed = {
+            email: user.email,
+            cvrnr: user.cvrnr,
+            navn: user.navn,
+            adresse: user.adresse,
+            tlfnr: user.tlfnr,
+            hjemmeside: user.hjemmeside,
+            direktoer: user.direktoer,
+            land: user.land,
+            postnr: user.postnr,
+            by: user.by,
+            logo: user.logo
+        }
+    });
 
-      db.Uddannelser.findAll({
+
+    console.log(loggedInVirksomhed);
+
+    var generatedEducationOptions = '';
+
+    db.Uddannelser.findAll({
         order: [
-          ['name', 'ASC']
+            ['name', 'ASC']
         ]
-      }).then(result => {
+    }).then(result => {
         result.forEach(element => {
-          generatedEducationOptions += "<option value='" + element.dataValues.id + "'>" + element.dataValues.name + "</option>";
+            generatedEducationOptions += "<option value='" + element.dataValues.id + "'>" + element.dataValues.name + "</option>";
         });
         res.render('internship_post', {
-          title: 'Opret opslag',
-          linkRegex: tempLink,
-          dateRegex: tempDate,
-          emailRegex: tempEmail,
-          generatedEducationOptions: generatedEducationOptions
+            title: 'Opret opslag',
+            linkRegex: tempLink,
+            dateRegex: tempDate,
+            emailRegex: tempEmail,
+            generatedEducationOptions: generatedEducationOptions,
+            remail: loggedInVirksomhed.email,
+            raddress: loggedInVirksomhed.adresse,
+            rhomepage: loggedInVirksomhed.hjemmeside,
+            rpostcode: loggedInVirksomhed.postnr,
+            rcity: loggedInVirksomhed.by,
+            rcountry: loggedInVirksomhed.land,
+            rlogo: loggedInVirksomhed.logo
         });
-      }).catch();
-});
+    }).catch();
+})
+;
 
 module.exports = router;
