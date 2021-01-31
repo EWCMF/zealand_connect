51bf92a4bdfdda9e6ec79be8009b84f2ea0379fd
Benjamin Kyhn <benkyhn@hotmail.com>
2021-01-31 20:06:52 +0100

    Oprettet en folder til middlewares og authorizeUser tager nu imod et array af user roles

--- nodes/middlewares/authorizeUser.js
+++ nodes/middlewares/authorizeUser.js
@@ -1,13 +1,13 @@
 // Autorisérer brugeren når man skal give adgang til funktioner, der kun er for specifikke roller
-function authorizeUser (userRole) {
+function authorizeUser (...permittedRoles) {
     return (req, res, next) => {
         if (req.user == null) {
             return res.status(403).render('error403', {layout: false});
-        } else if (userRole === 'student' && res.locals.isStudent) {
+        } else if (permittedRoles.includes('student') && res.locals.isStudent) {
             next();
-        } else if (userRole === 'company' && res.locals.isCompany) {
+        } else if (permittedRoles.includes('company') && res.locals.isCompany) {
             next();
-        } else if (userRole === ' admin' && res.locals.isAdmin){
+        } else if (permittedRoles.includes('admin') && res.locals.isAdmin){
             next();
         } else {
             return res.status(403).render('error403', {layout: false});
