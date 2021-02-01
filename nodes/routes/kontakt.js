6e2700d7f7fc08f4ef85a70e1262b54267ea1528
Benjamin Kyhn <benkyhn@hotmail.com>
2021-02-01 17:20:15 +0100

    Fikset reqLang import i kontakt.js

--- nodes/routes/kontakt.js
+++ nodes/routes/kontakt.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {reqLang} = require('../public/javascript/request');
 
 
 router.get('/', function(req, res, next) {
