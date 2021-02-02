488b133b87b692a92f503c548f172461a4c2270a
Tommy <54972963+EWCMF@users.noreply.github.com>
2021-02-02 02:20:21 +0100

    Tilføjet ændrer password til virksomhedsprofil

--- nodes/routes/profil.js
+++ nodes/routes/profil.js
@@ -15,9 +15,13 @@ const mv = require('mv');
 const {
     reqLang
 } = require('../public/javascript/request');
-const { validatePhone, validateName } = require("../validation/input-validation");
+const {
+    validatePhone,
+    validateName
+} = require("../validation/input-validation");
 const checkForIdenticals = require('../validation/input-validation').checkForIdenticals;
 const validatePasswordLength = require('../validation/input-validation').validatePasswordLength;
+const authorizeUser = require("../middlewares/authorizeUser").authorizeUser;
 
 router.get('/', function (req, res, next) {
     findUserByEmail(req.user).then((user) => {
@@ -36,6 +40,7 @@ router.get('/', function (req, res, next) {
                 logo: user.logo,
                 ejer: true
             }
+            console.log(loggedInVirksomhed)
             res.render('visprofil', {
                 language: reqLang(req, res),
                 loggedInVirksomhed
@@ -58,7 +63,7 @@ router.get('/', function (req, res, next) {
 
 router.get('/virksomhed/:id', async function (req, res) {
     let id = req.params.id;
-    
+
     let json = await models.Virksomhed.findByPk(id, {
         raw: true
     });
@@ -158,10 +163,24 @@ router.post('/redigerstudent-save', function (req, res) {
     formData.parse(req, async function (error, fields, files) {
         //laver et objekt med alle data
         const {
-            email, fornavn, efternavn, telefon, profile_picture, crop_base64, password, gentagPassword
+            email,
+            fornavn,
+            efternavn,
+            telefon,
+            profile_picture,
+            crop_base64,
+            password,
+            gentagPassword
         } = fields;
         let content = {
-            email, fornavn, efternavn, telefon, profile_picture, crop_base64, password, gentagPassword
+            email,
+            fornavn,
+            efternavn,
+            telefon,
+            profile_picture,
+            crop_base64,
+            password,
+            gentagPassword
         };
 
         const imageBufferData = Buffer.from(crop_base64, 'base64');
@@ -188,66 +207,66 @@ router.post('/redigerstudent-save', function (req, res) {
             //Kombinere oprindelig filnavn med unik data for at lave unike filnavne.
             let newPicName = datetime + randomNumber + "_" + img.name;
 
-                if (imgData.width >= 250 && imgData.height >= 250) {
-                    if (size <= 1000000) {
-                        //Når filer bliver uploaded bliver de lagt i en midlertigt mappe med tilfældignavn.
-                        //Nedenstående flytter og omdøber filer på sammetid
-                        if (img.type === "image/jpeg" || img.type === "image/png" || img.type === "image/svg+xml" || img.type === "image/bmp") {
-                            let tempPath = path.join(os.tmpdir(), Date.now() + '');
+            if (imgData.width >= 250 && imgData.height >= 250) {
+                if (size <= 1000000) {
+                    //Når filer bliver uploaded bliver de lagt i en midlertigt mappe med tilfældignavn.
+                    //Nedenstående flytter og omdøber filer på sammetid
+                    if (img.type === "image/jpeg" || img.type === "image/png" || img.type === "image/svg+xml" || img.type === "image/bmp") {
+                        let tempPath = path.join(os.tmpdir(), Date.now() + '');
 
-                            //Gemmer buffer til en midlertidig fil i temp mappen med tilfældigt navn.
-                            //Dette er nødvendigt da man ikke kan bruge mv med buffer dataen direkte.
-                            fs.writeFileSync(tempPath, imageBufferData, function (err) {
-                                if (err) {
-                                    return res.error(err);
-                                }
-                            });
+                        //Gemmer buffer til en midlertidig fil i temp mappen med tilfældigt navn.
+                        //Dette er nødvendigt da man ikke kan bruge mv med buffer dataen direkte.
+                        fs.writeFileSync(tempPath, imageBufferData, function (err) {
+                            if (err) {
+                                return res.error(err);
+                            }
+                        });
 
-                            mv(tempPath, publicUploadFolder + newPicName, (errorRename) => {
-                                if (errorRename) {
-                                    console.log("Unable to move file.");
-                                } else {
-                                    models.Student.findOne({
-                                        where: {
-                                            email: email
-                                        }
-                                    }).then(result => {
-                                        if (result.profilbillede !== null) {
-                                            // Search the directory for the old profile picture
-                                            fs.readdir(uploadFolder, function (err, list) {
-                                                if (err) throw err;
-                                                for (let i = 0; i < list.length; i++) {
-                                                    // If the old profile picture exists, delete it
-                                                    if (list[i] === result.profilbillede) {
-                                                        unlinkOldFiles(result["profilbillede"])
-                                                    }
-                                                }
-                                            });
-                                        }
-                                    }).catch();
-                                    content.profile_picture = newPicName;
-
-                                    // Edit the students information
-                                    if (password && password === gentagPassword && validatePasswordLength(password) && checkForIdenticals(password, gentagPassword)) {
-                                        editPassword(email, password);
-                                        //TODO: Error handling here and change password for companies
+                        mv(tempPath, publicUploadFolder + newPicName, (errorRename) => {
+                            if (errorRename) {
+                                console.log("Unable to move file.");
+                            } else {
+                                models.Student.findOne({
+                                    where: {
+                                        email: email
                                     }
-                                    editStudent(email, fornavn, efternavn, telefon, content.profile_picture);
-                                    res.redirect('/profil/rediger');
+                                }).then(result => {
+                                    if (result.profilbillede !== null) {
+                                        // Search the directory for the old profile picture
+                                        fs.readdir(uploadFolder, function (err, list) {
+                                            if (err) throw err;
+                                            for (let i = 0; i < list.length; i++) {
+                                                // If the old profile picture exists, delete it
+                                                if (list[i] === result.profilbillede) {
+                                                    unlinkOldFiles(result["profilbillede"])
+                                                }
+                                            }
+                                        });
+                                    }
+                                }).catch();
+                                content.profile_picture = newPicName;
+
+                                // Edit the students information
+                                if (password && password === gentagPassword && validatePasswordLength(password) && checkForIdenticals(password, gentagPassword)) {
+                                    editPassword(email, password);
+                                    //TODO: Error handling here and change password for companies
                                 }
-                            });
-                        } else {
-                            console.log("invalid file");
-                            res.redirect('/profil/rediger');
-                        }
+                                editStudent(email, fornavn, efternavn, telefon, content.profile_picture);
+                                res.redirect('/profil/rediger');
+                            }
+                        });
                     } else {
-                        console.log("invalid filesize");
+                        console.log("invalid file");
                         res.redirect('/profil/rediger');
                     }
                 } else {
-                    console.log("Invalid image dimensions")
+                    console.log("invalid filesize");
                     res.redirect('/profil/rediger');
                 }
+            } else {
+                console.log("Invalid image dimensions")
+                res.redirect('/profil/rediger');
+            }
         } else {
             // Intet profilbillede, så nøjes med at opdatere de andre felter
             if (password && password === gentagPassword) {
@@ -265,10 +284,32 @@ router.post('/rediger-save', function (req, res, next) {
     formData.parse(req, async function (error, fields, files) {
         //laver et objekt med alle data
         const {
-            email, telefon, by, postnr, cvrnr, navn, address, hjemmeside, direktoer, land, profile_picture, crop_base64
+            email,
+            telefon,
+            by,
+            postnr,
+            cvrnr,
+            navn,
+            address,
+            hjemmeside,
+            direktoer,
+            land,
+            profile_picture,
+            crop_base64
         } = fields;
         let content = {
-            email, telefon, by, postnr, cvrnr, navn, address, hjemmeside, direktoer, land, profile_picture, crop_base64
+            email,
+            telefon,
+            by,
+            postnr,
+            cvrnr,
+            navn,
+            address,
+            hjemmeside,
+            direktoer,
+            land,
+            profile_picture,
+            crop_base64
         };
 
 
@@ -392,6 +433,59 @@ router.get('/getUser', function (req, res, next) {
     findUserByEmail(req.user).then((user) => {
         res.send(user);
     })
+});
+
+router.post('/change-password-company', authorizeUser('company'), async function (req, res, next) {
+    const {
+        hashPassword,
+        verifyPassword
+    } = require('../encryption/password');
+
+    var formData = new formidable.IncomingForm();
+    formData.parse(req, async function (error, fields, files) {
+
+        let oldPass = fields.gamlePassword;
+        let newPass = fields.nytPassword;
+        let repeatPass = fields.gentagNytPassword;
+
+        let errors = [];
+
+        let id = res.locals.user.id;
+
+        let virksomhed = await models.Virksomhed.findByPk(id, {
+            raw: true,
+            attributes: ["password"]
+        });
+
+        let passwordFromDb = virksomhed.password;
+
+        if (!await verifyPassword(oldPass, passwordFromDb)) {
+            errors.push(1);
+        }
+
+        if (!validatePasswordLength(newPass)) {
+            errors.push(2);
+        }
+
+        if (!checkForIdenticals(newPass, repeatPass)) {
+            errors.push(3);
+        }
+
+        if (errors.length > 0) {
+            return res.status(400).send(JSON.stringify(errors));
+        }
+
+        models.Virksomhed.update({
+            password: await hashPassword(newPass)
+        }, {
+            where: {
+                id: id
+            }
+        });
+
+        res.status(200).send('ok');
+    });
+
 });
 
 function unlinkOldFiles(filename) {
