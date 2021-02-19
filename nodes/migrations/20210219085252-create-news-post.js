aa0dc15884a6ffeacf8a5e9b11920a800674c472
Benjamin Kyhn <benkyhn@hotmail.com>
2021-02-19 10:27:54 +0100

    Tilføjet model til nyhedsindlæg

---
+++ nodes/migrations/20210219085252-create-news-post.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('NewsPosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      date: {
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('NewsPosts');
+  }
+};
--- nodes/routes/news.js
+++ nodes/routes/news.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { reqLang } = require('../public/javascript/request');
+const Models = require('../models');
 
-router.get('/', function(req, res, next){
-    res.render('nyheder', {language: reqLang(req, res)});
+router.get('/', async function(req, res, next){
+    const newsPosts = await Models.NewsPost.findAll({
+        raw: true
+    });
+
+    res.render('nyheder', {language: reqLang(req, res), json: newsPosts});
 });
 
 module.exports = router;
