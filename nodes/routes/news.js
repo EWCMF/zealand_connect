7034cc10d89ffa4ae3b78749d1f4ab95107a7728
Benjamin Kyhn <benkyhn@hotmail.com>
2021-02-19 10:59:19 +0100

    Tilføjet en route til at lave et nyt nyhedsindlæg

--- nodes/routes/news.js
+++ nodes/routes/news.js
@@ -2,13 +2,21 @@ const express = require('express');
 const router = express.Router();
 const { reqLang } = require('../public/javascript/request');
 const Models = require('../models');
+const authorizeUser = require("../middlewares/authorizeUser").authorizeUser;
 
 router.get('/', async function(req, res, next){
     const newsPosts = await Models.NewsPost.findAll({
         raw: true
     });
 
-    res.render('nyheder', {language: reqLang(req, res), json: newsPosts});
+    res.render('news', {language: reqLang(req, res), json: newsPosts});
 });
+
+router.get('/new-post', authorizeUser('admin'), async function(req, res, next){
+
+    res.render('news_post', {language: reqLang(req, res)});
+});
+
+
 
 module.exports = router;
