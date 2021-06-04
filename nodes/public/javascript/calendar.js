36bb234d7fc242de78790b88dd677ab1a48b0fd6
Tommy <54972963+EWCMF@users.noreply.github.com>
2021-06-04 15:42:43 +0200

    Edit og show i samme modal

--- nodes/public/javascript/calendar.js
+++ nodes/public/javascript/calendar.js
@@ -9,11 +9,15 @@ function getEvents(calendar){
                 start: event.startDate,
                 end: event.endDate,
                 description: event.description,
+                allDay: event.allday,
                 formattedStartDate: event.formattedStartDate,
                 formattedEndDate: event.formattedEndDate,
             }
             if (event.url) {
-                formattedEvent.url = event.url
+                formattedEvent.url = event.url;
+            }
+            if (event.location) {
+                formattedEvent.location = event.location;
             }
             calendar.addEvent(formattedEvent)
         }
@@ -35,7 +39,68 @@ function openEvent(info){
             document.getElementById("eventURL").innerHTML = null;
             document.getElementById("eventURL").href = null;
         }
+        document.getElementById("eventLocation").hidden = false;
+        document.getElementById('eventLocationLabel').hidden = false;
+        document.getElementById("eventLocation").innerHTML = null;
+        if (info.event.extendedProps.location) {
+            document.getElementById("eventLocation").innerHTML = info.event.extendedProps.location;
+        } else {
+            document.getElementById("eventLocation").hidden = true;
+            document.getElementById('eventLocationLabel').hidden = true;
+        }
+        document.getElementById('modalEditButton').onclick = null;
+        document.getElementById('modalEditButton').onclick = function () {
+            passInfoToEdit(info);
+        }
         openEventModal()
+}
+
+function passInfoToEdit(info) {
+    document.getElementById('modal-content').innerHTML = document.getElementById('eventModalCreate').innerHTML;
+
+    let startDate = new Date(info.event.start);
+
+    console.log(info);
+    
+    let formattedStartDate = startDate.getFullYear() + "-" + zeroPadDate(startDate.getMonth() + 1) 
+    + (startDate.getMonth() + 1) + "-" + zeroPadDate(startDate.getDate()) + startDate.getDate();
+
+    let endDate = new Date(info.event.end);
+
+    let formattedEndDate = endDate.getFullYear() + "-" + zeroPadDate(endDate.getMonth() + 1) 
+    + (endDate.getMonth() + 1) + "-" + zeroPadDate(endDate.getDate()) + endDate.getDate();
+
+    document.getElementById('startDate').value = formattedStartDate;
+    document.getElementById('endDate').value = formattedEndDate;
+
+    let startTime = zeroPadDate(startDate.getUTCHours()) + startDate.getUTCHours() + ':' + zeroPadDate(startDate.getMinutes()) + startDate.getMinutes();
+    let endTime = zeroPadDate(endDate.getUTCHours()) + endDate.getUTCHours() + ':' + zeroPadDate(endDate.getMinutes()) + endDate.getMinutes();
+
+    console.log(startTime);
+
+    document.getElementById('formStartTime').value = startTime;
+    document.getElementById('formEndTime').value = endTime;
+
+    if (info.event.allDay) {
+        document.getElementById('allDay').checked = true;
+        greyOutTime();
+    }
+
+    document.getElementById('formTitle').value = info.event.title;
+
+    if (info.event.extendedProps.location) {
+        document.getElementById('location').value = info.event.extendedProps.location
+    }
+
+    if (info.event.url) {
+        document.getElementById('url').value = info.event.url
+    }
+
+    document.getElementById('description').value = info.event.extendedProps.description;
+}
+
+function zeroPadDate(number) {
+    return number > 9 ? '' : '0';
 }
 
 function openEventModal() {
@@ -56,21 +121,33 @@ function createEvent(info, admin){
         return;
     }
 
-    console.log(isAdmin)
+    $(document).ready(async function () {
 
-    $(document).ready(function () {
-        $('#createEventModal').modal({
+        document.getElementById('modal-content').innerHTML = document.getElementById('eventModalCreate').innerHTML;
+
+        $('#eventModal').modal({
             backdrop: 'static',
             keyboard: false
         });
 
-        $("#createEventModal").modal('show');
+        $("#eventModal").modal('show');
 
         document.getElementById('startDate').value = info.dateStr
 
-        $('#createEventModal').on('hidden.bs.modal', function (e) {
-            document.getElementById('eventForm').reset();
-            greyOutTime()
+        $('#eventModal').on('hidden.bs.modal', function (e) {
+            document.getElementById('modal-content').innerHTML = document.getElementById('eventModalShow').innerHTML;
         })
     });
+}
+
+function greyOutTime() {
+    let checked = document.getElementById('allDay').checked;
+
+    if (checked) {
+        document.getElementById('formStartTime').value = "";
+        document.getElementById('formEndTime').value = "";
+        document.getElementById('timeArea').classList.add('disable-form');
+    } else {
+        document.getElementById('timeArea').classList.remove('disable-form');
+    }
 }
