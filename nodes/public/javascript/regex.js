487fd74530423d5c2439d9e0015d036f6f0ef891
Tommy <54972963+EWCMF@users.noreply.github.com>
2021-06-13 08:01:31 +0200

    Forbedrede linkRegex med regex fra quill-magic-url da den gamle havde fejl

--- nodes/public/javascript/regex.js
+++ nodes/public/javascript/regex.js
@@ -4,7 +4,7 @@ const cvrRegex = /^[0-9]{8}$/
 const phoneRegex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/
 const cityRegex = /^[a-zA-Z æøåÆØÅ]*$/
 const dateRegex = /^\d{4}[.\/-]\d{2}[.\/-]\d{2}$/
-const linkRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
+const linkRegex = /^(https?:\/\/|www\.)[\w-\.]+\.[\w-\.]+(\/([\S]+)?)?$/i
 const nameRegex = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
 const numbersRegex = /^[0-9]{8}$/;
 const postcodeRegex = /^[0-9]{4}$/;
