271951cdd41c954ff396972951ff3e8f8f320fe2
Benjamin Kyhn <benkyhn@hotmail.com>
2021-06-17 11:34:25 +0200

    Fix city regex

--- nodes/constants/regex.js
+++ nodes/constants/regex.js
@@ -2,7 +2,7 @@
 const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,6}$/
 const cvrRegex = /^[0-9]{8}$/
 const phoneRegex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/
-const cityRegex = /^[a-zA-Z æøåÆØÅ]*$/
+const cityRegex = /^[^\d]*$/
 const dateRegex = /^\d{4}[.\/-]\d{2}[.\/-]\d{2}$/
 const linkRegex = /^(https?:\/\/|www\.)[\w-\.]+\.[\w-\.]+(\/([\S]+)?)?$/i
 const nameRegex = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
